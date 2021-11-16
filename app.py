fc8356c27b4cdd5a67cd896179dc8a627275e700
Dan Cripe <dan@lumenovis.com>
2021-11-15 21:09:19 -0800

    ENS lookup; improved handling of non-displayable tokens; removed accidental checkin of shadow dir and zip

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from flask import render_template, request
-from jinja2 import TemplateNotFound
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/')
+@app.after_request
+def set_headers(response):
+    return response
+
+
+
+@app.route('/', methods=['GET'])
 def index():
     print("serving index.htlm")
-    return render_template('index.html'), 404
+    return render_template('index.html'), 200
+
 
 
 if __name__ == '__main__':
